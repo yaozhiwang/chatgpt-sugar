5a152aa6704d0d12aadf86ca1018bfa133bce990
Yaozhi Wang <wangyaozhiyz@gmail.com>
2023-12-25 22:28:39 +0800

    [Framework] Add tailwindcss
    https://tailwindcss.com/docs/guides/vite

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="read-the-docs underline">
         Click on the Vite and React logos to learn more
       </p>
     </>
