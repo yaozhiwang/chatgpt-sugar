fd169a96f473d3e9adcf483c573a9ac68e661f43
Yaozhi Wang <wangyaozhiyz@gmail.com>
2023-12-28 16:07:16 +0800

    [Journey] convert timeline to png

--- src/content-script/chatgpt-journey/journey/index.tsx
+++ src/content-script/chatgpt-journey/journey/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 
 import { Dialog } from "@/components/dialog"
 import { Share } from "@/components/icons"
+import { useToPng } from "@hugocxl/react-to-image"
 
 import { JourneyData } from "../data"
 import Timeline from "./timeline"
@@ -11,6 +12,12 @@ export default function Journey() {
   const [journeyData, setJourneData] = useState<JourneyData>()
 
   const [openDialog, setOpenDialog] = useState(false)
+  const [imageData, setImageData] = useState<string>()
+  const [state, convertToPng, timelineRef] = useToPng<HTMLDivElement>({
+    onSuccess: (data) => {
+      setImageData(data)
+    }
+  })
 
   return (
     <div className="text-token-text-primary absolute left-0 top-0 z-30 w-full bg-white dark:bg-gray-800">
@@ -25,7 +32,9 @@ export default function Journey() {
       <div className="mx-auto justify-center px-4 py-2 text-base md:gap-6">
         <div className="mx-auto flex flex-1 gap-3 text-base md:max-w-3xl md:px-5 lg:max-w-[40rem] lg:px-1 xl:max-w-[48rem] xl:px-5">
           {journeyData ? (
-            <Timeline data={journeyData} />
+            <div ref={timelineRef}>
+              <Timeline data={journeyData} />
+            </div>
           ) : (
             <Waiting onGenerated={setJourneData} />
           )}
@@ -33,13 +42,36 @@ export default function Journey() {
       </div>
       {openDialog && (
         <Dialog
-          children={<p>hah</p>}
+          children={
+            <ShareDialog convertToPng={convertToPng} imageData={imageData} />
+          }
           title="Share your journey"
           onClose={() => {
             setOpenDialog(false)
           }}
         />
       )}
+    </div>
+  )
+}
+
+function ShareDialog({
+  convertToPng,
+  imageData
+}: {
+  convertToPng: () => void
+  imageData?: string
+}) {
+  return (
+    <div className="flex flex-col">
+      {imageData && <img src={imageData} />}
+      <button
+        className="btn btn-primary relative m-auto"
+        onClick={() => {
+          convertToPng()
+        }}>
+        Download image
+      </button>
     </div>
   )
 }
