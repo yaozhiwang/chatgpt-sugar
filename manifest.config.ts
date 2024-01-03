c105578156fd2187c06f2bdf757c728170775c33
Yaozhi Wang <wangyaozhiyz@gmail.com>
2024-01-03 17:34:17 +0800

    [Sugar] Add add-on ID for firefox AMO

--- manifest.config.ts
+++ manifest.config.ts
@@ -10,24 +10,34 @@ const description =
 
 export default defineManifest(async (env) => {
   return {
-    manifest_version: 3,
-    name: env.mode !== "production" ? `[DEV] ${name}` : name,
-    description,
-    version,
-    icons: {
-      "16": "src/assets/logo.png",
-      "32": "src/assets/logo.png",
-      "48": "src/assets/logo.png",
-      "128": "src/assets/logo.png"
+    ...{
+      manifest_version: 3,
+      name: env.mode !== "production" ? `[DEV] ${name}` : name,
+      description,
+      version,
+      icons: {
+        "16": "src/assets/logo.png",
+        "32": "src/assets/logo.png",
+        "48": "src/assets/logo.png",
+        "128": "src/assets/logo.png"
+      },
+      action: {},
+      host_permissions: ["https://*.openai.com/"],
+      permissions: [],
+      content_scripts: [
+        {
+          matches: ["https://chat.openai.com/*"],
+          js: ["src/content-script/index.tsx"]
+        }
+      ]
     },
-    action: {},
-    host_permissions: ["https://*.openai.com/"],
-    permissions: [],
-    content_scripts: [
-      {
-        matches: ["https://chat.openai.com/*"],
-        js: ["src/content-script/index.tsx"]
+    ...(process.env.BROWSER === "firefox" && {
+      browser_specific_settings: {
+        gecko: {
+          id: "firefox@chatgptsugar.xyz",
+          strict_min_version: "42.0"
+        }
       }
-    ]
+    })
   }
 })
